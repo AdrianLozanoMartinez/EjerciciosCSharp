9d604ab69b7942fe776712046097291b0d59e51e
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-15 09:56:59 +0100

    Initial commit

---
+++ EjerciciosCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ejercicio01
+{
+    class Program
+    {
+        /*Ejercicio 1
+            El programa preguntará el nombre al usuario y a continuación le saludará de la siguiente forma "Hola, NOMBRE" es el nombre del usuario.
+            Intentar que aparezca con comillas*/
+
+        static void Main()
+        {
+            Console.WriteLine("¿Cuál es su nombre?"); //Muestra: ¿Cuál es su nombre?
+            string name = Console.ReadLine();         //Coge lo introducido por teclado en formato string.
+
+            Console.WriteLine($"\"Hola, {name}\"");   //Muestra: "Hola, nombre_de_la_persona"
+        }
+    }
+}
