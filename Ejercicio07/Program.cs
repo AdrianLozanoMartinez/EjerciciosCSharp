5be1bd5555e4932d95aaea26a3a6af035a885e1b
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-15 12:26:07 +0100

    Ejercicio 07

---
+++ Ejercicio07/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio07
+{
+    class Program
+    {
+        /*Ejercicio 7.
+            El programa pedirá al usuario que introduzca 3 números enteros. Cada número
+            corresponderá a la dimensión de un lado de un triángulo. Por pantalla se mostrará si el
+            triángulo es equilátero, isósceles o escaleno.
+            */
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
