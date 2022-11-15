5a4055099bd690821513ccfc512368302146db23
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-15 10:38:40 +0100

    Ejercicio 2

--- Ejercicio02/Program.cs
+++ Ejercicio02/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Ejercicio02
+﻿using System.Globalization;  //Para que funcione el . en lugar de la , en el decimal
+
+namespace Ejercicio02
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        /*Ejercicio 2
+             El programa pedirá que se introduzca dos números reales y mostrará a continuación cuál es su suma.*/
+
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;   //Para que funcione el . en lugar de la , en el decimal
+
+            Console.WriteLine("Introduzca dos números reales");
+
+            Console.WriteLine("1º número real");
+            double num1 = double.Parse(Console.ReadLine());             //Transformamos en double para usar decimales/reales
+
+            Console.WriteLine("2º número real");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double suma = num1 + num2;
+
+            Console.WriteLine($"La suma es {suma}");
         }
     }
 }
