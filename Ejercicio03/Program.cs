cb3cb6e57367cf89bf69cbb800ca804ce4fb3af2
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-15 11:19:20 +0100

    Ejercicio03

---
+++ Ejercicio03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio03
+{
+    class Program
+    {
+        /*Ejercicio 3
+          El programa pedirá al usuario que introduzca una temperatura en grados Fahrenheit y, a continuación, mostrará esa 
+          temperatura traducida a grados Celsius
+
+          NOTA: Para pasar de ºF a ºC primero hemos de restar 32 y, a continuación, dividir entre 9 y multiplicar por 5.*/
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
