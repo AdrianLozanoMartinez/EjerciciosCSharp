9770417033db1a7f1e3951a3c5039fab029d93e9
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-15 12:19:03 +0100

    Ejercicio 06

---
+++ Ejercicio06/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Ejercicio06
+{
+    class Program
+    {
+        /*Ejercicio 6
+            Simplificar el siguiente segmento de algoritmo de forma que se necesiten menos comparaciones:
+
+            if (age >= 65)
+            {
+                Console.WriteLine("Jubilado");
+            }
+            else
+            {
+                if(age < 18)
+                {
+                    Console.WriteLine("Menor de edad");
+                }
+                else
+                {
+                    if ((age >=18) && (age < 65))
+                    {
+                        Console.WriteLine("Activo");
+                    }
+                }
+            }
+             */
+
+        static void Main()
+        {
+            Console.WriteLine("Introduzca una edad");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 65)
+            {
+                Console.WriteLine("Jubilado");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("Menor de edad");
+            }
+            else
+            {
+              Console.WriteLine("Activo");
+            }
+        }
+    }
+}
