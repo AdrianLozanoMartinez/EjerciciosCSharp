7c1297591620fb8d664c700d46bcfc7e565bf092
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-16 13:19:08 +0100

    Ejercicio 14

---
+++ Ejercicio14/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio14
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Ejercicio 14.
+            El programa pedirá al usuario dos números enteros. El primer número será la semilla para el
+            generador aleatorio y el segundo será la cantidad de caracteres que se van a generar. Se
+            tendrá que pasar cada número generado a carácter ASCII y mostrarlo por pantalla.*/
+
+            Console.WriteLine("Introduzca un número entero para la semilla");
+            int seed = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca un número entero para la cantidad de caracteres");
+            int amount = int.Parse(Console.ReadLine());
+
+            Random random= new Random(seed);
+            int randomNumber = 0;
+            char ascii = ' ';
+
+            for (int i = 0; i < amount; i++)
+            {
+                randomNumber = random.Next(256);
+                ascii = (char)randomNumber;
+
+                Console.WriteLine($"Número aleatorio: {randomNumber} - Número ASCII: {ascii}");
+            }
+        }
+    }
+}
