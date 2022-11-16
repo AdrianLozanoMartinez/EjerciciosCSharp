029858a2d3f20a7056537e30fb9a60e465bc7da2
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-16 12:59:58 +0100

    Ejercicios 11,12,13 añadidos

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejercicio11
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Ejercicio 11.
+            El programa pedirá al usuario un número entero y mostrará todos los números primos
+            dentro del rango 1 y el número dado*/
+
+            Console.WriteLine("Introduzca un número entero");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= num; i++) //El número 1 no es primo y por eso se omite
+            {
+                bool isPrime = true;
+
+                //Comprobamos desde el 2 hasta i / 2 así como máximo un número se va a poder dividir entre su mitad
+                for (int j = 2; j <= i / 2 && isPrime; j++)  //Dividimos /2 para coger el divisor. 
+                {                                            //Mientras se pueda dividir por 2 y sea true, hará la comprobación
+                    if (i % j == 0)
+                    {
+                        isPrime = false;  //Si da resto no es primo, ya que buscamos no dividir entre 1 y entre sí mismo (por eso dividimos entre 2)
+                    }
+                }
+
+                if (isPrime) //Si es true se imprime número. 
+                {
+                    Console.WriteLine(i); //Muestra si no da resto 0 números que no sean 1 ni si mismo y por eso es primo
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio13/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio13
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Ejercicio 13.
+            El programa pedirá al usuario un número entero, posteriormente sacará 5 números
+            aleatorios dentro del rango de 1 y el número dado, cada número aleatorio se mostrará por
+            pantalla y se indicará al usuario si el número que ha dado coincide con alguno de los
+            generados (sólo se notificará al usuario de la coincidencia la primera vez, si coincide más
+            veces no se notificará)*/
+
+            Console.WriteLine("Introduzca un número entero");
+            int num = int.Parse(Console.ReadLine());
+
+            Random random= new Random();
+
+            int numRandom = 0;
+
+            bool exit = false;                         //Salir si coincide una vez
+
+            for (int i = 0; i < 5; i++)                //Manejar solo 5 números aleatorios
+            {
+                numRandom = random.Next( 1, num + 1 ); //Números aleatorios desde el 1 hasta el número dado +1,
+                                                       //ya que el random coge un número menos del que ponemos
+                //Console.WriteLine(numRandom);
+
+                                     //!exit //2º forma en lugar de exit == false
+                if (numRandom == num && exit == false) //Si un número aleatorio coincide con el introducido y además
+                {                                      //la condición es false como inicialmente marcado, hasta que 
+                                                       //entre en el if que se cambia por true, para mostrar solo 1
+                    Console.WriteLine("Coincide");
+
+                    exit = true;                       //De este modo se muestra solo 1 vez el mensaje ya que no 
+                                                       //puede entrar de nuevo en el if al ser exit=false
+                }
+            }
+
+        }
+    }
+}
